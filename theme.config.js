5e13786b239276fe94c52783e97cdddcfcc03403
Farzam <farzam@antarctica.local>
2023-07-10 00:14:08 +0300

    Every thing reach to first step

---
+++ theme.config.js
@@ -0,0 +1,10 @@
+const YEAR = new Date().getFullYear()
+
+export default {
+  footer: (
+    <medium style={{ display: 'block', marginTop: '6rem' }}>
+      <time>{YEAR}</time> via &#9650;
+    </medium>
+
+  )
+}
